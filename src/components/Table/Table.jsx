5f1094f5149cd9488cc1b374e32c7e31308a1977
Huda Ayaz <hudaayaz21@gmail.com>
2023-06-15 23:05:14 -0400

    Try adding and binding appropriate functions, still working

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,6 +4,14 @@ import TableRow from "../TableRow/TableRow.jsx";
 export default class Table extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      rowNum: 1,
+      colNum: 1
+    }
+
+    this.addRow = this.addRow.bind(this);
+    this.addCol = this.addCol.bind(this);
   }
 
   render() {
@@ -12,5 +20,17 @@ export default class Table extends Component {
         <TableRow />
       </div>
     );
+  }
+
+  addRow(event) {
+    this.setState({
+      rowNum: this.state.rowNum + 1
+    })
+  }
+
+  addCol(event) {
+    this.setState({
+      colNum: this.state.colNum + 1
+    })
   }
 }
