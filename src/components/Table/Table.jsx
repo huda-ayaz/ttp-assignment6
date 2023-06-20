fd9d0929f55b19fca35a12fb5c7eb6c987470dfd
Huda Ayaz <hudaayaz21@gmail.com>
2023-06-20 05:20:57 -0400

    Add select color and change color upon cell click functionalities, but there is a bug that forces a double click to initiate the first color change that needs to be fixed

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,13 +2,13 @@ import React, { useState, Component, ReactPropTypes } from "react";
 import { useEffect } from "react";
 import TableRow from "../TableRow/TableRow.jsx";
 
-const Table = ({ rowNum, colNum }) => {
+const Table = ({ rowNum, colNum, colorVal }) => {
   const [rows, setRows] = useState(1);
 
   const repeatRows = () => {
     const rowsArray = Array.from({ length: rowNum }, (_, index) => index);
     return rowsArray.map((item) => (
-      <TableRow colNum={colNum}/>
+      <TableRow colNum={colNum} colorVal={colorVal}/>
     ));
   };
 
--- src/components/TableCell/TableCell.jsx
+++ src/components/TableCell/TableCell.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./TableCell.css";
 
-const TableCell = () => {
+const TableCell = ({ colorVal }) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [color, setColor] = useState("beige");
+
+  const handleColorChange = () => {
+    setIsSelected(true);
+    console.log(isSelected);
+    console.log("selected");
+    style();
+  }
+
+  const style = () => {
+    if (isSelected) {
+      console.log("made it change")
+      setColor(colorVal);
+    } else {
+      console.log("made it beige")
+      setColor("beige");
+    }
+  }
+
   return (
-    <td></td>
+    <td style={{backgroundColor:color}} id="cell" onClick={handleColorChange}></td>
   );
 }
 
